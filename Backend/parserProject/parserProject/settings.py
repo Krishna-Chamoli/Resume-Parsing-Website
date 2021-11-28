5c008e97294a71e3ca4a4e4b564381de039d1e1c
Krishna Chamoli <krishnachamoli290@gmail.com>
2021-11-28 21:36:26 +0530

    updated code

--- Backend/parserProject/parserProject/settings.py
+++ Backend/parserProject/parserProject/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r#0hb#08ecj1d4b5k4opu#djf^jv6i@9&38ev@e59_g-f-hi1t'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Backend/parserProject/sendSms.py
+++ Backend/parserProject/sendSms.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from decouple import config
 
 def sendMessage(mobile):
     url = "https://www.fast2sms.com/dev/bulkV2"
@@ -11,10 +12,11 @@ def sendMessage(mobile):
         'numbers': mobile    
     }
     headers = {
-        'authorization': 'qcWbsB3DYpj1mnrUgNKSEzQ8k0fyO4JeGa95TwCF62utXoAlPVgsVQwzauhe97BASOG3YKWJRM08X2I1',
+        'authorization': config("authorization"),
         'Content-Type': "application/x-www-form-urlencoded",
         'Cache-Control': "no-cache"
     }
     response = requests.request("POST", url, data = my_data, headers = headers)
     returned_msg = json.loads(response.text)
     print(returned_msg['message'])
+
